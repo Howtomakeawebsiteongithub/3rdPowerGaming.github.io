c9dcb5d4664000f001736b91d9749b72b30e60d0
3rdPowerGaming <77739185+3rdPowerGaming@users.noreply.github.com>
2021-06-06 18:51:19 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+ function changeBodyBg(color){
+        document.body.style.background = color;
+    }
+   
+   
+   function myFunction{
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ); 
+   }
