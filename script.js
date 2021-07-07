25e8ab11a01a8220688547a620afb9d41479f7d8
3rdPowerGaming <77739185+3rdPowerGaming@users.noreply.github.com>
2021-07-07 19:48:58 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
- function changeBodyBg(color){
-        document.body.style.background = color;
-    }
-   
-   
-   function myFunction(){
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; 
-   }
